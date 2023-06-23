7b6e3efe920b1015d1edb4a11e7136c795f992d2
ThiagoAssalin <thiagos.assalin@gmail.com.com>
2023-06-23 15:20:15 -0300

    BUILD - configurando axios para requisiçoes de api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import Header from "@/components/header/header";
 
 export default function Home() {
   return (
-    <main >
+    <main className="bg-indigo-950 h-screen text-white">
       <Header/>
+      <div className=" w-full p-3 " >
+        <h2>Meu Mural</h2>
+        
+      </div>
     </main>
   )
 }
---
+++ src/components/posts/posts.tsx
@@ -0,0 +1,11 @@
+ import api from "@/api/api";
+ 
+ export default function Posts(){
+    return(
+        <div id="posts">
+            <div id="card-title"></div>
+            <div id="card-description"></div>
+        </div>
+    )
+
+ }
